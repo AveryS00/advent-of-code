7980e3a74177e331b3b70ba47b345e08f5758383
AveryS00 <ajsmith2@wpi.edu>
2020-12-03 13:11:21 -0500

    real one line

--- 2020/day3/small.py
+++ 2020/day3/small.py
@@ -1,5 +1,1 @@
-# fuck around and shorten it I guess
-# What can't I do with list comprehension, reduce, and the newly learned walrus operator?
-from functools import reduce
-from math import ceil
-print(reduce(lambda a, b: a*b, [reduce(lambda c, d: c+d, [([ceil(delta_x * len(arr) / delta_y / len(arr)) * line for line in arr][y_pos][x_pos] == '#') for x_pos, y_pos in zip(range(0, 10000000, delta_x), range(0, len((arr := [line.rstrip('\n') for line in open('input.txt')])), delta_y))]) for delta_x, delta_y in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]], 1))
+print(__import__('functools').reduce(lambda a, b: a*b, [__import__('functools').reduce(lambda c, d: c+d, [([__import__('math').ceil(delta_x * len(arr) / delta_y / len(arr[0])) * line for line in arr][y_pos][x_pos] == '#') for x_pos, y_pos in zip(range(0, 10000000, delta_x), range(0, len([line.rstrip('\n') for line in open('input.txt')]), delta_y)) if (arr := [line.rstrip('\n') for line in open('input.txt')])]) for delta_x, delta_y in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]], 1))
