ba41091f5000622bf28fdbed82f706781617e3ee
AveryS00 <ajsmith2@wpi.edu>
2020-12-03 00:28:48 -0500

    day3

---
+++ 2020/day3/main.py
@@ -0,0 +1,27 @@
+from math import ceil
+
+
+def num_trees(delta_x, delta_y, tree_map):
+    width = len(tree_map[0])
+    depth = len(tree_map)
+    full_map = [ceil(delta_x * (depth/delta_y) / width) * line for line in arr]
+
+    x_pos = 0
+    y_pos = 0
+    count = 0
+    while y_pos < depth:
+        count += (full_map[y_pos][x_pos] == '#')
+        x_pos += delta_x
+        y_pos += delta_y
+    return count
+
+
+with open('input.txt') as f:
+    arr = [line.rstrip('\n') for line in f]
+
+deltas = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+total = 1
+for x, y in deltas:
+    total *= num_trees(x, y, arr)
+
+print(total)
