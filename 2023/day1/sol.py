dbbdbdfb73ae24cdb8b318dffcf845fc39b279c3
AveryS00 <as@averysmith.net>
2023-12-02 15:29:16 -0500

    [2023] Day1 Part2

--- 2023/day1/sol.py
+++ 2023/day1/sol.py
@@ -1,17 +1,37 @@
+import re
 from typing import Union
 
 
+conversions: dict[str, int] = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9
+}
+p = re.compile(r'(one|two|three|four|five|six|seven|eight|nine|[1-9])')
+p_rev = re.compile(r'(?s:.*)(one|two|three|four|five|six|seven|eight|nine|[1-9])')
+
+
 def parse_line(line: str) -> int:
-    first: Union[int, None] = None
-    last: Union[int, None] = None
-    for c in line:
-        try:
-            val: int = int(c)
-        except ValueError:
-            continue
-        if first is None:
-            first = val
-        last = val
+    print(line, end='')
+    m = p.search(line)
+    try:
+        first: int = int(m.group(1))
+    except ValueError:
+        first = conversions[m.group(1)]
+    print(first)
+    m = p_rev.search(line)
+    try:
+        last: int = int(m.group(1))
+    except ValueError:
+        last = conversions[m.group(1)]
+    print(last)
+
     return first * 10 + last
 
 
@@ -21,4 +41,5 @@ with open('input.txt', 'r') as f:
     for line in f:
         values.append(parse_line(line))
 
+print(values)
 print(sum(values))
